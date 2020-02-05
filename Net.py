873c33a1f8d366d874c50befdf0410e25d656dcc
domgan <ganczarski.dominik@gmail.com>
2020-02-05 16:11:25 +0100

    new net arch

--- Net.py
+++ Net.py
@@ -3,22 +3,24 @@ import Feature_Train
 import Feature_Test
 
 model = keras.Sequential([
-    keras.layers.Conv2D(128, (5, 5), input_shape=(20, 16, 1), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3), input_shape=(20, 16, 1), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    keras.layers.Conv2D(256, (3, 3), activation='relu'),
+    keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    keras.layers.Conv2D(128, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
     keras.layers.Flatten(),
-    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(256, activation='relu'),
     keras.layers.Dense(6, activation='softmax')
     ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer=keras.optimizers.Adam(lr=0.0001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(Feature_Train.train_input, Feature_Train.train_labels, epochs=4)
+model.fit(Feature_Train.train_input, Feature_Train.train_labels, epochs=5)
 
 results = model.evaluate(Feature_Test.test_input, Feature_Test.test_labels)
 print('test loss, test acc:', results)
